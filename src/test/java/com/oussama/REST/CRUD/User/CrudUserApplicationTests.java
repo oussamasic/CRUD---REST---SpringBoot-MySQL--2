23ad7ed63380dbe55338968809022886b00f20ea
Zerouali <oussamazerouali42@gmail.com>
2018-03-01 23:28:47 +0000

    After running the project

---
+++ src/test/java/com/oussama/REST/CRUD/User/CrudUserApplicationTests.java
@@ -0,0 +1,16 @@
+package com.oussama.REST.CRUD.User;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CrudUserApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
