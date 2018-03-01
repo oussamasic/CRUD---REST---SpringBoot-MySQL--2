ae59e930df418fe18e6559368d23e0644b5935bb
Zerouali <oussamazerouali42@gmail.com>
2018-03-01 23:27:52 +0000

    create the repository

---
+++ src/main/java/com/oussama/REST/CRUD/User/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.oussama.REST.CRUD.User.repository;
+
+import com.oussama.REST.CRUD.User.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    // les méthodes save(), findOne(), findAll(), count(), delete() sont generées automatiquement par le JPA
+
+}
