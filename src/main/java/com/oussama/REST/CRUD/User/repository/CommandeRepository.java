509cbc27f27b1cdf81e91b1c2d6fc669ac5c0221
Zerouali <oussamazerouali42@gmail.com>
2018-03-02 22:31:19 +0000

    Create Commande repository

--- src/main/java/com/oussama/REST/CRUD/User/repository/CommandeRepository.java
+++ src/main/java/com/oussama/REST/CRUD/User/repository/CommandeRepository.java
@@ -1,4 +1,9 @@
 package com.oussama.REST.CRUD.User.repository;
 
-public interface CommandeRepository {
+import com.oussama.REST.CRUD.User.model.Commande;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommandeRepository extends JpaRepository<Commande, Long> {
 }
