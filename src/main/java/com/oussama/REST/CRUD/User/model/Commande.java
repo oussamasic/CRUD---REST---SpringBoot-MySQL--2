e198b07fa20b67b41a74764b074c9377b34bade0
Zerouali <oussamazerouali42@gmail.com>
2018-03-02 22:31:05 +0000

    Create Commande Model

--- src/main/java/com/oussama/REST/CRUD/User/model/Commande.java
+++ src/main/java/com/oussama/REST/CRUD/User/model/Commande.java
@@ -1,4 +1,20 @@
 package com.oussama.REST.CRUD.User.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "commandes")
 public class Commande {
+
+    //table commande(id_commande,name, prix)
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "prix")
+    private String prix;
 }
